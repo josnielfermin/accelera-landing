0a145ca811ab4e6c223d8d8a51267c48a9ec82ad
josniel <josnieldavidfb18@gmail.com>
2025-04-23 10:55:40 -0400

    new fixes

--- components/Scene/Accelera.tsx
+++ components/Scene/Accelera.tsx
@@ -2,14 +2,14 @@ import SplineViewer from '@/components/Landing/Spline';
 
 const Accelera = () => {
   return (
-    <div className="absolute h-[649px] w-[649px] max-xl2:w-[440px] max-xl2:h-[440px] lg:bottom-[30px] xl2:bottom-[20px] max-lg:top-[140px] xl:right-[300px] lg:right-[260px] min-[1600px]:right-[300px] right-0 mix-blend-lighten xl2:overflow-hidden max-lg:overflow-hidden">
-      <div className="rounded-full blur-[250px] w-[960px] h-[960px] [background:radial-gradient(rgba(0,_0,_0,_0.90),_rgba(0,_0,_0,_0))] absolute bottom-0 left-1/2 -translate-x-1/2 z-[-1]"></div>
+    <div className="absolute z-[2] h-[649px] w-[649px] max-xl2:w-[440px] max-xl2:h-[440px] lg:bottom-[30px] xl2:bottom-[20px] max-lg:top-[140px] xl:right-[300px] lg:right-[260px] min-[1600px]:right-[300px] right-0 mix-blend-lighten xl2:overflow-hidden max-lg:overflow-hidden">
+      {/* <div className="rounded-full blur-[250px] w-[960px] h-[960px] [background:radial-gradient(rgba(0,_0,_0,_0.90),_rgba(0,_0,_0,_0))] absolute bottom-0 left-1/2 -translate-x-1/2 z-[-1]"></div> */}
       {/* <SplineViewer
         url="https://prod.spline.design/fA3jwoAwjAbouj0u/scene.splinecode"
         className="w-full h-full object-contain relative z-[1] bottom-[-50px] max-xl2:bottom-[85px] max-[1870px]:right-10 max-lg:top-0 max-xxs:top-6 max-lg:right-0 max-sm:-right-20 max-xs:-right-40 max-xxs:-right-52 max-[280px]:-right-60"
       /> */}
       <video
-        className="w-full h-full object-contain relative z-[4] bottom-[-30px] max-xl2:bottom-[65px] max-[1870px]:right-10 max-lg:top-0 max-xxs:top-6 max-lg:right-0 max-sm:-right-20 max-xs:-right-40 max-xxs:-right-52 max-[280px]:-right-60"
+        className="w-full h-full object-contain relative bottom-[-30px] max-xl2:bottom-[65px] max-[1870px]:right-10 max-lg:top-0 max-xxs:top-6 max-lg:right-0 max-sm:-right-20 max-xs:-right-40 max-xxs:-right-52 max-[280px]:-right-60"
         style={{ mixBlendMode: 'lighten' }}
         autoPlay
         loop
